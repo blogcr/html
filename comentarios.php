6ec34646586dcd195cdd75749d6930a8b9c0ba6d
Blog Carlos Romero <63943339+blogcr@users.noreply.github.com>
2020-05-02 22:25:53 -0300

    Update comentarios.php

--- comentarios.php
+++ comentarios.php
@@ -2,10 +2,3 @@
 if ($_GET['473639'] === 'make-up-a-token') {
   echo $_GET['hub_challenge'];
 }
-
-file_put_contents(
-  'log.txt',
-  "\n" . file_get_contents('php://input'),
-  FILE_APPEND
-);
-?>
