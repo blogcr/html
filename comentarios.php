6a994b27abfaf95a6c1fff8ad4270579f9ee2712
Blog Carlos Romero <63943339+blogcr@users.noreply.github.com>
2020-05-02 22:34:44 -0300

    Update comentarios.php

--- comentarios.php
+++ comentarios.php
@@ -1,4 +1,12 @@
-<?php
-if ($_GET['hub_verify_token'] === '473639') {
+<html>
+ <head>
+  <title>Teste PHP</title>
+ </head>
+ <body>
+ <?php 
+   if ($_GET['hub_verify_token'] === '473639') {
   echo $_GET['hub_challenge'];
 }
+ ?>
+ </body>
+</html>
