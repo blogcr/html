ec1bdd91f2e6a5419473b3147514b198e5f2c4a3
Blog Carlos Romero <63943339+blogcr@users.noreply.github.com>
2020-05-08 21:32:43 -0300

    Update scroll.js

--- scroll.js
+++ scroll.js
@@ -2,9 +2,9 @@ $(document).ready(function() {
 
 $(window).scroll(function() {
 
-  if ($(window).scrollTop() > 500) { $('#at4-share').hide();
+  if ($(window).scrollTop() > 500) { $('#at4-share').show();
   }
-  if ($(window).scrollTop() < 500) { $('#at4-share').show();
+  if ($(window).scrollTop() < 500) { $('#at4-share').hide();
   }
 });
 });
